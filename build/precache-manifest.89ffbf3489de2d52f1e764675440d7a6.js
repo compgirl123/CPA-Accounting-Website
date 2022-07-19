a69897e8a3653879e94e43f1dae84ffa0a92cbb6
compgirl123 <claudia.f.feochari@hotmail.com>
2022-07-19 00:23:39 -0400

    build

---
+++ build/precache-manifest.89ffbf3489de2d52f1e764675440d7a6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb94ffacfbb9c2370160f4ee0b04efca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bc59949ccdf8988b625",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "8363917e1dc25a0ddbfe",
+    "url": "/static/css/main.a3f2d8f0.chunk.css"
+  },
+  {
+    "revision": "1bc59949ccdf8988b625",
+    "url": "/static/js/2.c9586278.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.c9586278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8363917e1dc25a0ddbfe",
+    "url": "/static/js/main.722eb085.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "270e36b7654b910843c7a75808673058",
+    "url": "/static/media/2022-02-24.270e36b7.png"
+  },
+  {
+    "revision": "f3a1c1051baae298fb0f08337b302d47",
+    "url": "/static/media/BouchaibPic.f3a1c105.jpg"
+  },
+  {
+    "revision": "988a235ed4df5a880283c7955350d757",
+    "url": "/static/media/SoumayaPic.988a235e.jpg"
+  }
+]);
